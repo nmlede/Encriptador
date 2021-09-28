ecfb83c5e789b3efa9d3a5cf79895d801c4acf77
Nicolas <nmlede1988@gmail.com>
2021-09-28 16:41:14 -0300

    Programa con error en la opcion 2 cuando no se ha seleccionado la opcion 1 antes

---
+++ Programa.py
@@ -0,0 +1,84 @@
+from cryptography.fernet import Fernet
+
+
+# FUNCION PARA ESCRIBIR Y GUARDAR CLAVE.
+def generarClave():
+    clave = Fernet.generate_key()
+    with open("clave.key","wb") as archivoClave:
+        archivoClave.write(clave)
+        
+
+# FUNCION PARA ESCRIBIR Y GUARDAR MENSAJE.
+def generarMensaje():
+    mensaje = input("Ingrese el mensaje original: ")
+    with open("mensaje.txt","w") as archivoMensaje:        
+        archivoMensaje.write(mensaje)
+        
+
+# FUNCION PARA CARGAR CLAVE.
+def cargarClave():
+    return open("clave.key","rb").read()
+
+
+# FUNCION PARA CARGAR MENSAJE.
+def cargarMensaje():
+    return open("mensaje.txt","rb").read()
+
+
+# FUNCION PARA ESCRIBIR Y GUARDAR MENSAJE ENCRIPTADO.
+def generarEncriptado():
+    encriptado = fernet.encrypt(mensaje)
+    with open("encriptado.txt","wb") as archivoEncriptado:
+        archivoEncriptado.write(encriptado)
+
+# FUNCION PARA CARGAR MENSAJE ENCRIPTADO.
+def cargarEncriptado():
+    return open("encriptado.txt","rb").read()
+
+
+
+print("Programa ENCRIPTADOR/DESENCRIPTADOR")
+
+otraOpcion = "y"
+
+while (otraOpcion == "y"):
+
+    while True:
+
+        try:
+            selector = int(input("\nSeleccione una opcion:\n1- Encriptar.\n2- Desencriptar.\n"))
+            if(selector == 1):
+                generarClave()
+                generarMensaje()                
+                clave = cargarClave()
+                mensaje = cargarMensaje()
+                fernet = Fernet(clave)
+                generarEncriptado()
+                encriptado = cargarEncriptado()
+                print("Mensaje ENCRIPTADO: \n",encriptado)
+                break
+            
+            elif(selector == 2):
+                encriptado = cargarEncriptado()    
+                clave = cargarClave()
+                fernet = Fernet(clave)
+                desencriptado = fernet.decrypt(encriptado)
+                print("Mensaje DESENCRIPTADO: \n",desencriptado)
+                break
+            
+            else:
+                print("Opcion incorrecta, intente nuevamente")
+        
+        except ValueError:
+            print("Opcion incorrecta, intente nuevamente")
+    
+    otraOpcion = input("Desea seleccionar otra opcion? (y/n): ")
+
+print("\nFin del programa")
+
+"""
+TODO: La opcion 1 deberia crear un archivo para almacenar el mensaje original, para que ese mismo
+archivo sea usado por la opcion 2 en el caso que solo necesitemos desencriptar. De esta forma
+se cumple el requisito de que el receptor tenga la clave y el mensaje encriptado para 
+poder desencriptar.
+"""
